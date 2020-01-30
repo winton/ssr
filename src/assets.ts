6dff76995e166e30836feab83c303edd5e73a7f4
winton <mail@wintoni.us>
2020-01-29 19:59:07 -0800

    Assets helper

---
+++ src/assets.ts
@@ -0,0 +1,58 @@
+import { join } from "path"
+import globby from "globby"
+import { readFile } from "fs-extra"
+
+export const EXT_REGEX = /(.+)(\.[^\.]+)$/
+
+export default async function(
+  path: string
+): Promise<[number, string, string] | void> {
+  const match = path.match(EXT_REGEX)
+
+  if (!match || !match[2]) {
+    return
+  }
+
+  let [, name, ext] = match
+  let map = ""
+
+  if (ext === ".map") {
+    map = ext
+    ;[, name, ext] = name.match(EXT_REGEX)
+  }
+
+  const root = join(
+    __dirname,
+    __dirname.includes("/dist/") ? "../../" : "../"
+  )
+
+  const nameExt = process.env.STAGE ? "" : "-*"
+
+  const glob = [
+    join(root, path),
+    join(root, `${name}${nameExt}${ext}${map}`),
+    join(root, `${name}.js${map}`),
+  ]
+
+  const paths = await globby(glob)
+
+  if (paths[0]) {
+    const mjs = await readFile(paths[0])
+
+    let contentType = "text/javascript"
+
+    if (ext === ".css") {
+      contentType = "text/css"
+    }
+
+    if (ext === ".ts") {
+      contentType = "application/typescript"
+    }
+
+    if (map) {
+      contentType = "application/json"
+    }
+
+    return [200, contentType, mjs.toString()]
+  }
+}
---
+++ test/assets.spec.ts
@@ -0,0 +1,47 @@
+import expect from "./expect"
+import assets from "../src/assets"
+
+describe("assets", () => {
+  it("should return correct ts path", async () => {
+    let code: number, type: string
+    const asset = await assets("/test/assets.spec.ts")
+
+    if (asset) {
+      ;[code, type] = asset
+    }
+
+    expect(code).toBe(200)
+    expect(type).toBe("application/typescript")
+  })
+
+  it("should return void if not found", async () => {
+    const asset = await assets("/404")
+    expect(asset).toBeUndefined()
+  })
+
+  it("should return correct dist path", async () => {
+    let code: number, type: string
+    const asset = await assets("/dist/esm/index.mjs")
+
+    if (asset) {
+      ;[code, type] = asset
+    }
+
+    expect(code).toBe(200)
+    expect(type).toBe("text/javascript")
+  })
+
+  it("should return correct node_modules path", async () => {
+    let code: number, type: string
+    const asset = await assets(
+      "/node_modules/@fn2/loaded/dist/esm/index.mjs"
+    )
+
+    if (asset) {
+      ;[code, type] = asset
+    }
+
+    expect(code).toBe(200)
+    expect(type).toBe("text/javascript")
+  })
+})
